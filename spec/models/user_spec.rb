43949923a6e9e800fa2c09fcc8d7c81f3c2efc29
unknown <Usuario@.(none)>
2014-06-12 19:02:20 +0200

    branch33

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
 
 
   it { should be_valid }
